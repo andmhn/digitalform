520134ba3378d7568f888fe0869da5ed45ae9cec
Anand Mohan <andmhn@outlook.com>
2024-12-27 13:19:47 +0530

    add program arg to allow cors for all websites

--- form-api/src/main/java/com/github/andmhn/digitalform/DigitalFormApplication.java
+++ form-api/src/main/java/com/github/andmhn/digitalform/DigitalFormApplication.java
@@ -6,10 +6,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Objects;
+
 @SpringBootApplication
 public class DigitalFormApplication {
+	private static String allowedOrigin = "http://localhost:4200";
 
 	public static void main(String[] args) {
+		if(args.length > 0 && Objects.equals(args[0], "--all-cors")){
+			allowedOrigin = "*";
+		}
 		SpringApplication.run(DigitalFormApplication.class, args);
 	}
 
@@ -18,7 +24,7 @@ public class DigitalFormApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("GET", "POST", "PATCH", "DELETE");
+				registry.addMapping("/**").allowedOrigins(allowedOrigin).allowedMethods("GET", "POST", "PATCH", "DELETE");
 			}
 		};
 	}
