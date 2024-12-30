abf52b296d513f65c30a1ffe946f43215a334292
Anand Mohan <andmhn@outlook.com>
2024-12-30 12:05:42 +0530

    allow cors for websites with environment variables

--- form-api/src/main/java/com/github/andmhn/digitalform/DigitalFormApplication.java
+++ form-api/src/main/java/com/github/andmhn/digitalform/DigitalFormApplication.java
@@ -6,16 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.Objects;
-
 @SpringBootApplication
 public class DigitalFormApplication {
-	private static String allowedOrigin = "http://localhost:4200";
-
 	public static void main(String[] args) {
-		if(args.length > 0 && Objects.equals(args[0], "--all-cors")){
-			allowedOrigin = "*";
-		}
 		SpringApplication.run(DigitalFormApplication.class, args);
 	}
 
@@ -24,7 +17,10 @@ public class DigitalFormApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins(allowedOrigin).allowedMethods("GET", "POST", "PATCH", "DELETE");
+				String allowedOrigins = System.getenv("ALLOWED_ORIGINS") +  ",http://localhost:4200,";
+				registry.addMapping("/**")
+						.allowedOrigins(allowedOrigins.split(","))
+						.allowedMethods("GET", "POST", "PATCH", "DELETE");
 			}
 		};
 	}
